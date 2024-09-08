1d4e84f84c7d4ac2eaccf3eb2c865651347b4fdb
Nerea <163418422+NereaTovar@users.noreply.github.com>
2024-09-08 13:12:52 +0200

    Vercel

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: './', // Establece una ruta base relativa
+  build: {
+    outDir: 'dist', // Asegura que el directorio de salida sea `dist`
+    assetsDir: 'assets', // Para mantener los assets en un directorio llamado 'assets'
+  },
 })
