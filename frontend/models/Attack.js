d24e3299336dece718f532c17c020a67de0073df
Nerea <163418422+NereaTovar@users.noreply.github.com>
2024-09-17 19:31:20 +0200

    Mandatory

--- frontend/models/Attack.js
+++ frontend/models/Attack.js
@@ -1,17 +1,15 @@
-import mongoose from 'mongoose';
-
+import mongoose from "mongoose";
 
 const attackSchema = new mongoose.Schema({
   type: { type: String, required: true },
   intensity: { type: String, required: true },
   duration: { type: String },
-  invalidating: { type: Boolean, required: true },
-  medication: { type: Boolean, required: true },
-  menstruation: { type: Boolean, required: true },
+  invalidating: { type: String },
+  medication: { type: String },
+  menstruation: { type: String },
   date: { type: Date, default: Date.now },
 });
 
-
-const Attack = mongoose.model('Attack', attackSchema);
+const Attack = mongoose.model("Attack", attackSchema);
 
 export default Attack;
